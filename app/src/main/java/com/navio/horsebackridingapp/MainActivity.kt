79a472f373fc38d905ddf6f7bd83baee33b4eb5b
Jose Navío <josenavio747@gmail.com>
2024-06-03 22:05:05 +0200

    feat: User login working properly

--- app/src/main/java/com/navio/horsebackridingapp/MainActivity.kt
+++ app/src/main/java/com/navio/horsebackridingapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import android.util.AttributeSet
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setButtons()
+        disableButtons()
         attachLoginFragment()
     }
 
@@ -38,12 +40,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun attachLoginFragment() {
-
-        supportFragmentManager.commit {
-            setReorderingAllowed(true)//Let commit operations decide better operation's order
-            replace(binding.mainActivityContainer.id, FragmentLogin.newInstance())
+    val loginFragment = FragmentLogin.newInstance()
+    loginFragment.setLoginCallback(object : LoginCallback {
+        override fun onLoginSuccess() {
+            enableButtons()
+            attachBookingsFragment()
         }
+    })
+
+    supportFragmentManager.commit {
+        setReorderingAllowed(true)
+        replace(binding.mainActivityContainer.id, loginFragment)
     }
+}
 
     private fun attachBookingsFragment() {
 
@@ -51,5 +60,19 @@ class MainActivity : AppCompatActivity() {
             setReorderingAllowed(true)
             replace(binding.mainActivityContainer.id, FragmentBookings.newInstance())
         }
+    }
+
+    //Enable/Disable buttons
+    private fun enableButtons() {
+        binding.mainActivityButtonLogin.visibility = View.VISIBLE
+        binding.mainActivityButtonBookings.visibility = View.VISIBLE
+    }
+
+    private fun disableButtons() {
+        binding.mainActivityButtonLogin.visibility = View.GONE
+        binding.mainActivityButtonBookings.visibility = View.GONE
+    }
+    interface LoginCallback {
+        fun onLoginSuccess()
     }
 }
--- app/src/main/java/com/navio/horsebackridingapp/fragments/FragmentLogin.kt
+++ app/src/main/java/com/navio/horsebackridingapp/fragments/FragmentLogin.kt
@@ -7,11 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.navio.horsebackridingapp.MainActivity
 import com.navio.horsebackridingapp.R
 import com.navio.horsebackridingapp.api.RetrofitClient
 import com.navio.horsebackridingapp.data.LoginRequest
 import com.navio.horsebackridingapp.data.LoginResponse
+import com.navio.horsebackridingapp.fragments.bookings.FragmentBookings
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
@@ -23,11 +26,20 @@ class FragmentLogin : Fragment() {
     private lateinit var passwordEditText: EditText
     private lateinit var loginButton: Button
 
+    private var callback: MainActivity.LoginCallback? = null
+    fun setLoginCallback(callback: MainActivity.LoginCallback) {
+        this.callback = callback
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        return initComponents(inflater.inflate(R.layout.fragment_login, container, false))
+    }
+
+    private fun initComponents(view: View): View {
 
         usernameEditText = view.findViewById(R.id.fragment_login_edit_text_username)
         passwordEditText = view.findViewById(R.id.fragment_login_edit_text_password)
@@ -35,21 +47,49 @@ class FragmentLogin : Fragment() {
 
         //It is neccessary to create a network security configuration file so this app can access the site
         loginButton.setOnClickListener {
+
             val username = usernameEditText.text.toString().trim()
             val password = passwordEditText.text.toString().trim()
+
+            if(username.isEmpty()){
+                usernameEditText.error = "Username required..."
+                usernameEditText.requestFocus()
+                return@setOnClickListener
+            }
+
+            if(password.isEmpty()){
+                passwordEditText.error = "Password required..."
+                passwordEditText.requestFocus()
+                return@setOnClickListener
+            }
 
             val request = LoginRequest(username, password)
 
             RetrofitClient.instance.userLogin(request).enqueue(object : Callback<ResponseBody> {
 
-                override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
-                    val jsonResponse = response.body()?.string()
-                    Log.d("Navio_HorsebackRidingApp", "JSON Response: $jsonResponse")
+                override fun onResponse(
+                    call: Call<ResponseBody>,
+                    response: Response<ResponseBody>
+                ) {
+                    //Save the token in the shared preferences
+                    val response = response.body()?.string()
+
+                    if (response != WRONG_LOGIN){
+                        val sharedPref = activity?.getSharedPreferences("shared_preferences", 0)
+                        val editor = sharedPref?.edit()
+                        editor?.putString("token", response)
+                        editor?.apply()
+
+                        //Navigate to the bookings fragment
+                        callback?.onLoginSuccess()
+
+                    }else{
+                        Toast.makeText(context, response, Toast.LENGTH_LONG).show()
+                    }
                 }
 
                 override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                    // Handle the failure
-                    Log.d("Navio_Mal", t.message.toString())
+                    Toast.makeText(context, t.message, Toast.LENGTH_LONG).show()
                 }
             })
         }
@@ -58,6 +98,9 @@ class FragmentLogin : Fragment() {
     }
 
     companion object {
+
+        private const val WRONG_LOGIN = "Wrong..."
+
         @JvmStatic
         fun newInstance() = FragmentLogin()
     }
